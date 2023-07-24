757415824c6aebc60b3063ac80d2e8b5cc445ce0
qiyang20 <1079359761@qq.com>
2023-07-24 17:03:43 +0800

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <h1>测试</h1>
+  </div>
+</template>
+
+<script>
+import testVertex from '../shader/test/Vertex.glsl'
+console.log(testVertex)
+export default {
+  name: 'HomeView'
+}
+</script>
