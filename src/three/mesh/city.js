3d79355fbacf91db76d586fa0df8d5bdc44fbd81
qiyang20 <1079359761@qq.com>
2023-07-25 13:41:27 +0800

    修改城市的材质

--- src/three/mesh/city.js
+++ src/three/mesh/city.js
@@ -1,18 +1,20 @@
 import * as THREE from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
 import scene from '../scene'
+import modifyCityMaterial from '../modify/modifyCityMaterial'
 
 export default function craeteCity() {
   const gltfLoader = new GLTFLoader()
   gltfLoader.load('./model/city.glb', (gltf) => {
-    // console.log(gltf)
-    // 修改材质
     gltf.scene.traverse((item) => {
-      // console.log(item)
       if (item.type == 'Mesh') {
-        item.material = new THREE.MeshBasicMaterial({
-          color: new THREE.Color(0x00ffff)
+        // 对城市的金属材质进行修改
+        const cityMaterial = new THREE.MeshBasicMaterial({
+          color: new THREE.Color(0x0c0e33)
         })
+        item.material = cityMaterial
+        modifyCityMaterial(item)
       }
     })
     scene.add(gltf.scene)
---
+++ src/three/modify/modifyCityMaterial.js
@@ -0,0 +1,57 @@
+import * as THREE from 'three'
+// 修改城市的材质
+export default function modifyCityMaterial(mesh) {
+  // 获取高度差，最大值和最小值
+  mesh.geometry.computeBoundingBox()
+  console.log(mesh.geometry.boundingBox)
+  let { min, max } = mesh.geometry.boundingBox
+  let uHeight = max.y - min.y
+  // 编译之前
+  mesh.material.onBeforeCompile = (shader) => {
+    console.log(shader.vertexShader)
+    console.log(shader.fragmentShader)
+    // 传颜色
+    shader.uniforms.uTopColor = {
+      value: new THREE.Color('#aaaeff')
+    }
+    // 传高度差
+    shader.uniforms.uHeight = {
+      value: uHeight
+    }
+    shader.vertexShader = shader.vertexShader.replace(
+      '#include <common>',
+      `
+        #include <common>
+        varying vec3 vPosition;
+      `
+    )
+    shader.vertexShader = shader.vertexShader.replace(
+      '#include <begin_vertex>',
+      `
+        #include <begin_vertex>
+        vPosition = position;
+      `
+    )
+    shader.fragmentShader = shader.fragmentShader.replace(
+      '#include <common>',
+      `
+        #include <common>
+        uniform vec3 uTopColor;
+        uniform float uHeight;
+        varying vec3 vPosition;
+      `
+    )
+    shader.fragmentShader = shader.fragmentShader.replace(
+      '#include <dithering_fragment>',
+      `
+        #include <dithering_fragment>
+        vec4 distGradColor = gl_FragColor;
+        // 设置混合的百分比
+        float gradMix = (vPosition.y +uHeight /2.0) /uHeight;
+        // 计算出混合颜色
+        vec3 gradMinColor = mix(distGradColor.xyz,uTopColor,gradMix);
+        gl_FragColor = vec4(gradMinColor,1);
+      `
+    )
+  }
+}
