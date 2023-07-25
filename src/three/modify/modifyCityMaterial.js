404bb5d9cf10bca36561ee7d87b7a62fd6a0f946
qiyang20 <1079359761@qq.com>
2023-07-25 14:47:40 +0800

    从下到上扫描城市特效

--- src/three/modify/modifyCityMaterial.js
+++ src/three/modify/modifyCityMaterial.js
@@ -1,18 +1,29 @@
 import * as THREE from 'three'
+import gsap from 'gsap'
 // 修改城市的材质
 export default function modifyCityMaterial(mesh) {
   // 编译之前
   mesh.material.onBeforeCompile = (shader) => {
-    console.log(shader.vertexShader)
-    console.log(shader.fragmentShader)
-    gradColor(shader, mesh)
+    // console.log(shader.vertexShader)
+    // console.log(shader.fragmentShader)
+    shader.fragmentShader = shader.fragmentShader.replace(
+      '#include <dithering_fragment>',
+      `
+        #include <dithering_fragment>
+        //#end#
+    `
+    )
+    addGradColor(shader, mesh)
+    addSpread(shader)
+    addLightLine(shader)
+    addToTopLine(shader)
   }
 }
-
-export function gradColor(shader, mesh) {
+// 添加渐变
+export function addGradColor(shader, mesh) {
   // 获取高度差，最大值和最小值
   mesh.geometry.computeBoundingBox()
-  console.log(mesh.geometry.boundingBox)
+  // console.log(mesh.geometry.boundingBox)
   let { min, max } = mesh.geometry.boundingBox
   let uHeight = max.y - min.y
   // 传颜色
@@ -47,15 +58,137 @@ export function gradColor(shader, mesh) {
     `
   )
   shader.fragmentShader = shader.fragmentShader.replace(
-    '#include <dithering_fragment>',
+    '//#end#',
     `
-      #include <dithering_fragment>
       vec4 distGradColor = gl_FragColor;
       // 设置混合的百分比
       float gradMix = (vPosition.y +uHeight /2.0) /uHeight;
       // 计算出混合颜色
       vec3 gradMinColor = mix(distGradColor.xyz,uTopColor,gradMix);
       gl_FragColor = vec4(gradMinColor,1);
+      //#end#
+
     `
   )
+}
+
+// 添加建筑材质光波扩散特效
+export function addSpread(shader, center = new THREE.Vector2(0, 0)) {
+  // 设置扩散的中心点
+  shader.uniforms.uSpreadCenter = { value: center }
+  //   扩散的时间
+  shader.uniforms.uSpreadTime = { value: -2000 }
+  //   设置条带的宽度
+  shader.uniforms.uSpreadWidth = { value: 40 }
+
+  shader.fragmentShader = shader.fragmentShader.replace(
+    '#include <common>',
+    `
+      #include <common>
+
+      uniform vec2 uSpreadCenter;
+      uniform float uSpreadTime;
+      uniform float uSpreadWidth;
+      `
+  )
+
+  shader.fragmentShader = shader.fragmentShader.replace(
+    '//#end#',
+    `
+     float spreadRadius = distance(vPosition.xz,uSpreadCenter);
+    //  扩散范围的函数
+    float spreadIndex = -(spreadRadius-uSpreadTime)*(spreadRadius-uSpreadTime)+uSpreadWidth;
+
+    if(spreadIndex>0.0){
+        gl_FragColor = mix(gl_FragColor,vec4(1,1,1,1),spreadIndex/uSpreadWidth);
+    }
+
+    //#end#
+    `
+  )
+
+  gsap.to(shader.uniforms.uSpreadTime, {
+    value: 800,
+    duration: 3,
+    ease: 'none',
+    repeat: -1
+  })
+}
+
+// 添加光带扩散
+export function addLightLine(shader) {
+  //   扩散的时间
+  shader.uniforms.uLightLineTime = { value: -1500 }
+  //   设置条带的宽度
+  shader.uniforms.uLightLineWidth = { value: 200 }
+
+  shader.fragmentShader = shader.fragmentShader.replace(
+    '#include <common>',
+    `
+       #include <common>
+ 
+       
+       uniform float uLightLineTime;
+       uniform float uLightLineWidth;
+       `
+  )
+
+  shader.fragmentShader = shader.fragmentShader.replace(
+    '//#end#',
+    `
+     float LightLineMix = -(vPosition.x+vPosition.z-uLightLineTime)*(vPosition.x+vPosition.z-uLightLineTime)+uLightLineWidth;
+
+     if(LightLineMix>0.0){
+         gl_FragColor = mix(gl_FragColor,vec4(0.8,1.0,1.0,1),LightLineMix /uLightLineWidth);
+         
+     }
+ 
+     //#end#
+     `
+  )
+
+  gsap.to(shader.uniforms.uLightLineTime, {
+    value: 1500,
+    duration: 5,
+    ease: 'none',
+    repeat: -1
+  })
+}
+// 添加从下往上扫
+export function addToTopLine(shader) {
+  //   扩散的时间
+  shader.uniforms.uToTopTime = { value: 0 }
+  //   设置条带的宽度
+  shader.uniforms.uToTopWidth = { value: 40 }
+
+  shader.fragmentShader = shader.fragmentShader.replace(
+    '#include <common>',
+    `
+          #include <common>
+          
+          uniform float uToTopTime;
+          uniform float uToTopWidth;
+          `
+  )
+
+  shader.fragmentShader = shader.fragmentShader.replace(
+    '//#end#',
+    `
+        float ToTopMix = -(vPosition.y-uToTopTime)*(vPosition.y-uToTopTime)+uToTopWidth;
+    
+        if(ToTopMix>0.0){
+            gl_FragColor = mix(gl_FragColor,vec4(0.8,0.8,1,1),ToTopMix /uToTopWidth);
+            
+        }
+    
+        //#end#
+        `
+  )
+
+  gsap.to(shader.uniforms.uToTopTime, {
+    value: 500,
+    duration: 3,
+    ease: 'none',
+    repeat: -1
+  })
 }
